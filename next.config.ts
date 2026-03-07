38e6fcfd37cdf4988b5a0794b620e7fcd2f0cfb9
masrafi-000 <smmasrafi000@gmail.com>
2026-03-07 14:59:55 +0600

    feat: enable standalone output and console removal in Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
+
+  output: "standalone",
+  compiler:{
+    removeConsole: true,
+  },
+
   /* config options here */
   images: {
     remotePatterns: [
